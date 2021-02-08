300d5322aa9466d4c63d12fcb2ed301414d0445e
sassolig <gabriellasassoli@gmail.com>
2021-02-08 17:01:51 +0000

    modified exercise 10

--- exercise10.py
+++ exercise10.py
@@ -1,13 +1,12 @@
 
 sum = 0
 primes = []
-for i in range(2,2000000):
-    print(primes)
+for i in range(2,20000):
+
     for j in range(2, i):
         if not i % j:
             break
     else:
         primes.append(i)
         sum = sum + i
-print(sum
-      )
+print(sum)
